46174a76ec15eef87a04cbbaa46c13558cdee96d
Pedro Bahry <phbahry@gmail.com>
2024-09-05 15:47:58 -0300

    Commit agora vai

--- Solution/API/Program.cs
+++ Solution/API/Program.cs
@@ -1,12 +1,39 @@
 // Minimal APIs
 // Teste de APIs - Rest Client - Extensão VS Code
 // Postman/Insomnia outras aplicações externas para uso corporativo
+using API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Endpoints - Funcionalidade do Código
 // Request/Requisição - URL e o método/verbo HTTP
-app.MapGet("/", () => "Testando C#");
+app.MapGet("/", () => "API de Produtos");
+
+var produtos = new List<Produto>
+    {
+        new Produto { Id = "1", Nome = "Café", Preco = 10.50, Quantidade = 100, CriadoEm = DateTime.Now },
+        new Produto { Id = "2", Nome = "Leite", Preco = 5.20, Quantidade = 50, CriadoEm = DateTime.Now },
+        new Produto { Id = "3", Nome = "Açúcar", Preco = 3.80, Quantidade = 200, CriadoEm = DateTime.Now }
+    };
+
+//GET: /produto/listar
+app.MapGet("/produto/listar", () =>
+{
+    return produtos;
+});
+
+app.MapPost("/produto/cadastrar/{nome}/", (string nome) =>
+{
+    Produto produto = new Produto();
+    produto.Nome = "nome";
+    produtos.Add(produto);
+    return produtos;
+});
+
+// Corpo Requisição
+// Estudar e entender qual é o código HTTP adequado para um cadastro
+
 app.MapGet("/segundafunc", () => "Segunda funcionalidade");
 
 app.MapGet("/retornaendereco", () =>
@@ -21,7 +48,7 @@ app.MapGet("/retornaendereco", () =>
 
 app.MapGet("/retornacadastro", () =>
 {
-    var cadastro = new 
+    var cadastro = new
     {
         codigo = "1",
         nome = "Artur",
@@ -33,21 +60,17 @@ app.MapGet("/retornacadastro", () =>
 
 app.MapGet("/saudacao/{nome}", (string nome) => $"Olá, {nome}!");
 
-public class Item
-{
-    public string Produto { get; set; }
-    public int Quantidade { get; set; }
-}
+
 
 app.MapGet("/retornalist", () =>
 {
     var list = new
     {
         Id = 1, // Opcional
-        Cliente = new 
-        { 
-            Nome = "Maria", 
-            Email = "maria@gmail.com" 
+        Cliente = new
+        {
+            Nome = "Maria",
+            Email = "maria@gmail.com"
         },
         Itens = new List<Item>
         {
@@ -65,3 +88,13 @@ app.MapGet("/retornalist", () =>
 // - Guardar as informações em uma lista
 
 app.Run();
+
+Produto produto = new Produto();
+produto.Preco = 50;
+Console.WriteLine("Preço: " + produto.Preco);
+
+public class Item
+{
+    public string? Produto { get; set; }
+    public int Quantidade { get; set; }
+}
